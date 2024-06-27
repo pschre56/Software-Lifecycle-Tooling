f07fa7fc73637264412a92755f0d19903cd7146f
Pat <wi23b079@technikum-wien.at>
2024-06-27 16:36:09 +0200

    Player class and getmarker test added

---
+++ src/main/java/at/technikumwien/Player.java
@@ -0,0 +1,13 @@
+package at.technikumwien;
+
+class Player {
+    private char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
